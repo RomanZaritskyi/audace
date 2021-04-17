b50e663860df6ddff124187bb4ee5cc11a3bbdf3
RomanZaritskyi <romanzaritskyi@gmail.com>
2021-04-17 10:51:24 +0300

    add main part

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(function () {
+let button = document.querySelector('.model__text-open');
+let more = document.querySelector('.more');
+let text = document.querySelector('.model__text-body');
 
-})
+function addClass() {
+  more.classList.toggle('_active');
+  text.classList.toggle('_active')
+
+
+  if (more.classList.contains('_active')) {
+    button.innerHTML = "Скрыть"
+  }
+  else {
+    button.innerHTML = "Читать полностью.."
+  }
+}
+button.addEventListener('click', addClass);
+
+
